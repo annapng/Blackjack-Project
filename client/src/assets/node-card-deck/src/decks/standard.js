961dd7e2fdfcfb64c49f24ed0462558f6dd24eff
KateRitchie <ritchie_ecen@hotmail.com>
2023-03-23 21:20:32 -0600

    home page/app routing

--- client/src/assets/node-card-deck/src/decks/standard.js
+++ client/src/assets/node-card-deck/src/decks/standard.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 const Card = require('../card.js');
 const Rank = require('../rank.js');
--- client/src/assets/node-card-deck/src/rank.js
+++ client/src/assets/node-card-deck/src/rank.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 module.exports = class Rank {
 	constructor(_shortName, _longName, _sortNum) {
