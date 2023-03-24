961c5e0a84805289f2c8cc2d63181fc2ad6b57e8
Jacob Pickron <jpickron30@gmail.com>
2023-03-24 15:35:09 -0600

    Worked on routes

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,18 @@ function App() {
                 path="/" 
                 element={<Home />}         
               />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
             </Routes>
           </div>        
         </div>
