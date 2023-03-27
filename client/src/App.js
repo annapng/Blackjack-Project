02b6c72dce26d59851046d935f1998d8ec653a75
KateRitchie <ritchie_ecen@hotmail.com>
2023-03-27 17:24:44 -0600

    apollo added to client

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,42 @@ import Signup from './pages/Signup.js';
 import Profile from './pages/Profile.js';
 import Game from './pages/Game.js';
 
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-   <div className="flex-column justify-flex-start min-100-vh">
+    <ApolloProvider client={client}>
+    <Router>        
+    <div className="flex-column justify-flex-start min-100-vh">
           <div className="container">
             <Routes>
               <Route 
@@ -37,7 +65,8 @@ function App() {
             </Routes>
           </div>        
         </div>
-</Router>
+    </Router>
+    </ApolloProvider>
   );
 }
 
