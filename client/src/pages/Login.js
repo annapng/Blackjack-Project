d0fb58fc94c833f5c822a54ca6b49b6dc8cc677b
CaptureB <airdavid62@gmail.com>
2023-03-27 07:52:01 -0600

    Adding fetch for login information

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const LoginForm = () => {
+    const [username, setUserName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isSignUp, setIsSignUp] = useState(false);
@@ -8,9 +9,36 @@ const LoginForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
+        const data = { username, email, password };
+
+        fetch('/signup' , {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+
+        .then(response => { 
+        
+        })
+        .catch(error => {
+
+        })
+
         console.log(`Email: ${email}, Password: ${password}, isSignup: ${isSignUp}`);
 
     };
+
+    function handleUsernameChange(event) {
+        setUserName(event.target.value)
+    }
+
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
 
     return (
         <form onSubmit={handleSubmit}>
