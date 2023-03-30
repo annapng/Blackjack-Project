61192335cc8a50596a1ef39cf33ebf3abd903a75
Jacob Pickron <jpickron30@gmail.com>
2023-03-30 17:59:06 -0600

    got the header to work

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import Header from '../components/header/index.js';
 import StartGame from '../utils/Game/Blackjack.js';
 
 function Game() {
-    return <StartGame />;
-
+    return (
+    <>
+    <Header />
+    <br></br>
+    
+    <StartGame />;
+    </>
+    )
 };
 
 
