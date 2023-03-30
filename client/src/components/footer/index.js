0edccf6fe8c7763d4e9f9589d2a8c9b2626b82d4
CaptureB <airdavid62@gmail.com>
2023-03-30 14:31:49 -0600

    Added the footer to the home page, only being used in the home pages. Styled it

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../../../src/App.css';
 
 function Footer () {
-
-    //foot stuff
+    return (
+        <div className="footer">
+            <p> &copy; Over-Under21 2023</p>
+        </div>
+    )
+   
 }
 
 export default Footer;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 import logo from '../assets/logo.png';
 import '../App.css';
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Footer from '../components/footer';
 
 const Home = () => {
 return (
@@ -11,6 +12,7 @@ return (
       <div className="toggleButton">
         <Link className="login" to="/Login"> Login/Signup </Link>
       </div>
+      <Footer />
       </header>
     </div>
 )
