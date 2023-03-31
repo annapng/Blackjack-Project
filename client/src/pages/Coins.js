1e6d8cc222e80e0f84cc4bd428fe0450162eb42a
Jacob Pickron <jpickron30@gmail.com>
2023-03-30 18:20:55 -0600

    linked headers to diffrent pages

---
+++ client/src/pages/Coins.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/header";
+
+
+
+const Coins = () => {
+    return (
+        <>
+        <Header />
+        <br></br>
+
+
+        </>
+    )
+}
+
+
+export default Coins
